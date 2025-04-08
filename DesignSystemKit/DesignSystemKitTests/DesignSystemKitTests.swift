170482adeb9a79239c141716d0e73304a95dae96
Sharik <sharik.abubucker@skyconseil.fr>
2025-04-08 10:39:16 +0200

    first commit

---
+++ DesignSystemKit/DesignSystemKitTests/DesignSystemKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemKitTests.swift
+//  DesignSystemKitTests
+//
+//  Created by Sharik Mohamed on 08/04/2025.
+//
+
+import Testing
+@testable import DesignSystemKit
+
+struct DesignSystemKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
